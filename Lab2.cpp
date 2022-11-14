bea0e67e4bdd820df20be7031512b0951c2f3473
APELSIN4 <45320521+APSN4@users.noreply.github.com>
2022-11-14 08:56:43 +0300

    Update 2.0

--- Lab2.cpp
+++ Lab2.cpp
@@ -4,15 +4,27 @@
 
 using namespace std;
 
-long int ISeconds;
-int resultMinute;
-int resultHours;
-int resultdays;
+string Istrseconds;
+long ISeconds;
+long resultMinute;
+long resultHours;
+long resultdays;
+long resultYears; //31536000 second
+long resultDecade; //315360000 second
+long resultCentury; //3153600000 second
 
-int inputSeconds();
-string convertSecondsToMinute(int seconds);
+long inputSeconds();
+string convertSecondsToMinute(long seconds);
 string convertMinutesToHours(int minutes);
 string convertHoursToDays(int hours);
+string convertDaysToYears(int days);
+string convertYearToDecade(int years);
+string convertDecadeToCentury(int decade);
+string Convertseconds_minutes(long number1, string name);
+string ConvertHours(long number1, string name);
+string ConvertDays(long number1, string name);
+string resultOutput(long seconds, long minutes, long hours, long days, long years, long decades, long centuries);
+
 
 int main()
 {
@@ -21,16 +33,26 @@ int main()
     inputSeconds();
 }
 
-int inputSeconds()
+long inputSeconds()
 {
-    cin >> ISeconds;
-    resultMinute = stoi(convertSecondsToMinute(ISeconds));
+    cin >> Istrseconds;
+    ISeconds = atol(Istrseconds.c_str());
+    resultMinute = stol(convertSecondsToMinute(ISeconds));
     if (resultMinute >= 60)
         resultHours = stoi(convertMinutesToHours(resultMinute));
     if (resultHours >= 24)
         resultdays = stoi(convertHoursToDays(resultHours));
+    if (resultdays >= 365)
+        resultYears = stoi(convertDaysToYears(resultdays));
+    if (resultYears >= 10)
+        resultDecade = stoi(convertYearToDecade(resultYears));
+    if (resultDecade >= 10)
+        resultCentury = stoi(convertDecadeToCentury(resultCentury));
 
-    cout << resultdays << " " << resultHours << " " << resultMinute << " " << ISeconds << endl;
+    cout << resultCentury << " " << resultDecade << " " << resultYears <<
+        " " << resultdays << " " << ConvertDays(resultdays, "ден") << " " << resultHours << " " << ConvertHours(resultHours, "час") << " " << resultMinute <<
+        " " << Convertseconds_minutes(resultMinute, "минут") <<
+        " " << ISeconds << " " << Convertseconds_minutes(ISeconds, "секунд") << endl;
     /*
     if (resultMinute < 60) {
         if (resultMinute != 0 && ISeconds != 0)
@@ -44,7 +66,71 @@ int inputSeconds()
     return 0;
 }
 
-string convertSecondsToMinute(int seconds)
+string resultOutput(long seconds, long minutes, long hours, long days, long years, long decades, long centuries)
+{
+    if (seconds)
+        printf("hello");
+    return "3";
+}
+
+string Convertseconds_minutes(long number1, string name) {
+    string number_local;
+    int number;
+    number_local = to_string(number1);
+    number_local = number_local.back(); // взять последний символ
+    number = stol(number_local);
+    string local;
+    if (number == 1)
+        local = name + "а";
+    else if ((number >= 2 && number < 5) && !(number1 >= 11 && number1 <= 20))
+        local = name + "ы";
+    else if (number >= 5 || (number1 >= 11 && number1 <= 20))
+        local = name;
+    else if (number == 0 && (number1 != 0))
+        local = name;
+    else
+        local = "";
+    return local;
+}
+string ConvertHours(long number1, string name) {
+    string number_local;
+    int number;
+    number_local = to_string(number1);
+    number_local = number_local.back(); // взять последний символ
+    number = stol(number_local);
+    string local;
+    if (number == 1)
+        local = name;
+    else if ((number >= 2 && number < 5) && !(number1 >= 11 && number1 <= 20))
+        local = name + "а";
+    else if (number >= 5 || ((number1 >= 11) && (number1 <= 20)))
+        local = name + "ов";
+    else if (number == 0 && (number1 != 0))
+        local = name + "ов";
+    else
+        local = "";
+    return local;
+}
+
+string ConvertDays(long number1, string name) {
+    string number_local;
+    int number;
+    number_local = to_string(number1);
+    number_local = number_local.back(); // взять последний символ
+    number = stol(number_local);
+    string local;
+    if ((number == 1 || number == 21) && (!(number1 >= 11 && number1 <= 20)))
+        local = name + "ь";
+    else if (number >= 2 && number < 5 && (!(number1 >= 11 && number1 <= 20)))
+        local = "дня";
+    else if ((number >= 5) && (!(number1 >= 11 && number1 <= 20)) || ((number1 % 10 == 0) && (number1 != 0)) || (((number1 >= 11) && (number1 <= 20))))
+        local = "дней";
+    else
+        local = "";
+    return local;
+}
+
+string convertSecondsToMinute(long seconds)
 {
     int Minutes = 0;
     if (seconds >= 60)
@@ -96,4 +182,55 @@ string convertHoursToDays(int hours) {
             resultHours = abs(hours);
     }
     return to_string(days);
+}
+
+string convertDaysToYears(int days) {
+    int years = 0;
+    if (days >= 365) {
+        while (days >= 365)
+        {
+            days -= 365;
+            if (!(days < 0))
+                years++;
+            else if (days == 0)
+                years++;
+        }
+        if (days != 365)
+            resultdays = abs(days);
+    }
+    return to_string(years);
+}
+
+string convertYearToDecade(int years) {
+    int decade = 0;
+    if (years >= 10) {
+        while (years >= 10)
+        {
+            years -= 10;
+            if (!(years < 0))
+                decade++;
+            else if (years == 0)
+                decade++;
+        }
+        if (years != 10)
+            resultYears = abs(years);
+    }
+    return to_string(decade);
+}
+
+string convertDecadeToCentury(int decade) {
+    int Century = 0;
+    if (decade >= 10) {
+        while (decade >= 10)
+        {
+            decade -= 10;
+            if (!(decade < 0))
+                Century++;
+            else if (decade == 0)
+                Century++;
+        }
+        if (decade != 10)
+            resultDecade = abs(decade);
+    }
+    return to_string(Century);
 }
